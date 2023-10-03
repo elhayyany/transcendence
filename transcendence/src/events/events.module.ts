d6ff3231688f846da2797f71d07864d25a8fb2bc
elhayyany <elhayyanyabdelaziz@gmail.com>
2023-10-03 23:53:52 +0100

    working on nginx

---
+++ transcendence/src/events/events.module.ts
@@ -0,0 +1,11 @@
+import { Global, Injectable, Module } from '@nestjs/common';
+import { EventsGateway } from './events.gateway';
+import { ChatModule } from 'src/chat/chat.module';
+import { WsGuard } from 'src/auth/guard';
+
+@Global()
+@Module({
+  // imports: [WsGuard],
+  providers: [EventsGateway, ChatModule],
+})
+export class EventsModule {}
